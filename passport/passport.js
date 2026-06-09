e2303e8598a3923e755f15ad2eb59d8c966e4225
Samin Ahmed <saminahmed1001@gmail.com>
2026-06-09 20:59:21 +0600

    config: passwordjs

---
+++ passport/passport.js
@@ -0,0 +1,45 @@
+const db = require("../db/queires");
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const passwordUtils = require("../lib/passwordUtils");
+
+async function verifyCallback(username, password, done) {
+  try {
+    const user = await db.findUserByUsername(username);
+
+    if (!user) {
+      return done(null, false, { message: "No account was found" });
+    }
+
+    const isValid = await passwordUtils.validatePassword(password, user.hash);
+
+    if (isValid) {
+      return done(null, user);
+    } else {
+      return done(null, false, { message: "Incorrect password." });
+    }
+  } catch (error) {
+    return done(error, { message: "An error occured." });
+  }
+}
+
+const strategy = new LocalStrategy(verifyCallback);
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.user_id);
+});
+
+passport.deserializeUser(async (user_id, done) => {
+  const user = await db.findUserByID(user_id);
+  try {
+    if (user) {
+      done(null, user);
+    }
+  } catch (error) {
+    done(error);
+  }
+});
