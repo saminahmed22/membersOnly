d8f558267fa53c1b51c69bf24a9c795c5ca58c37
Samin Ahmed <saminahmed1001@gmail.com>
2026-06-12 17:41:31 +0600

    add controller for the index page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,8 +2,42 @@ const db = require("../db/queires");
 
 const { validationResult } = require("express-validator");
 
-function renderAllPosts(req, res) {
-  res.render("index");
+const userModel = require("../models/userModel");
+
+async function renderAllPosts(req, res) {
+  const isMember = req.user.status === "member";
+
+  const allPostData = isMember
+    ? await db.fetchAllPosts()
+    : await db.fetchAllPostsWithoutSecrets();
+
+  res.render("index", {
+    path: "homepage",
+    userdata: req.user,
+    allPostData,
+    postModalOpen: false,
+    secretModalOpen: false,
+    values: { title: "", desc: "" },
+    formValidationErrorMessages: [],
+  });
 }
 
-module.exports = { renderAllPosts };
+async function renderProfilePage(req, res) {
+  const userId = req.params.userId;
+  const accountData = await db.findUserByID(req.user.user_id);
+  const isMember = req.user.status === "member";
+  const data = await userModel.userData(userId, isMember);
+
+  res.render("index", {
+    path: "profilePage",
+    userdata: accountData,
+    posterdata: data.userdata,
+    postdata: data.postdata,
+    postModalOpen: false,
+    secretModalOpen: false,
+    values: { title: "", desc: "" },
+    formValidationErrorMessages: [],
+  });
+}
+
+module.exports = { renderAllPosts, renderProfilePage };
