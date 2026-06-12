5a16e7ed479414a57a9fd6bfea689d5fa2c14e28
Samin Ahmed <saminahmed1001@gmail.com>
2026-06-12 17:44:43 +0600

    Router for the site

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -9,6 +9,8 @@ const { validationResult } = require("express-validator");
 const indexController = require("../controllers/indexController");
 const uesrLoginController = require("../controllers/uesrLoginController");
 const userRegisterController = require("../controllers/userRegisterController");
+const postController = require("../controllers/postController");
+const secretController = require("../controllers/secretController");
 
 // middlewares
 const { isAuthenticated } = require("../middlewares/isAuthenticated");
@@ -16,6 +18,12 @@ const { validateLoginForm } = require("../middlewares/loginFormValidator");
 const {
   validateRegisterForm,
 } = require("../middlewares/registerFormValidator");
+
+const {
+  validateCreatePostForm,
+} = require("../middlewares/createPostFromValidator");
+
+const { validateSecretForm } = require("../middlewares/validateSecretForm");
 
 // Routes
 indexRouter.get("/", isAuthenticated, indexController.renderAllPosts);
@@ -73,6 +81,53 @@ indexRouter.post(
     next();
   },
   userRegisterController.registerUser,
+);
+
+// post routes
+indexRouter.post(
+  "/createPost",
+  isAuthenticated,
+  validateCreatePostForm,
+  (req, res, next) => {
+    const formValidationErrors = validationResult(req);
+
+    if (!formValidationErrors.isEmpty()) {
+      return postController.renderPostForm(req, res);
+    }
+
+    next();
+  },
+  postController.createPost,
+);
+
+indexRouter.get(
+  "/delete-post/:postId",
+  isAuthenticated,
+  postController.deletePost,
+);
+
+// Profile routes
+indexRouter.get(
+  "/user/:userId",
+  isAuthenticated,
+  indexController.renderProfilePage,
+);
+
+// Secret routes
+indexRouter.post(
+  "/join-council",
+  isAuthenticated,
+  validateSecretForm,
+  (req, res, next) => {
+    const formValidationErrors = validationResult(req);
+
+    if (!formValidationErrors.isEmpty()) {
+      return secretController.renderSecretForm(req, res);
+    }
+
+    next();
+  },
+  secretController.addToCouncil,
 );
 
 module.exports = indexRouter;
